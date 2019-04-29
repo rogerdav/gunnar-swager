6497e428b8c89afafd0d472d5b023ddbd86f11b5
Roger <roger.neil.davenport@gmail.com>
2019-04-29 16:02:51 -0700

    contact form working

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,12 +12,12 @@ const Contact = (props) => {
           <h5>Gunnar Swager Foundation</h5>
           <h5>5915 N Normandie St</h5>
           <h5>Spokane, WA 99205</h5>
-          <a href = "mailto: info@gunnarswagerfoundation.com">Send Email</a>
-          <h5>info@gunnarswagerfoundation.com</h5>
+          <a href = "mailto: info@gunnarswagerfoundation.com"><h5>info@gunnarswagerfoundation.com</h5></a>
+          {/* <h5>info@gunnarswagerfoundation.com</h5> */}
           <h5>509-939-2518</h5>
           <ContactForm />
-        
         </div>
+        
         <img src={require('../media/BD_website_01a_pb.jpg')} />
     </div>
   );
