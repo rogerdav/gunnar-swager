2835be8d76ef92579e7c095e1040ad33f7d44a20
Roger <roger.neil.davenport@gmail.com>
2020-01-14 14:08:06 -0800

    added 2nd video to media page

--- src/components/media.js
+++ src/components/media.js
@@ -5,13 +5,15 @@ import '../style/media.css';
 
 const Media = (props) => {
   return (
-    <div id="media" style={{ height: 800 }}>
+    <div id="media" 
+    style={{ height: 800 }}
+    >
       <img src={require('../media/BD_website_01a_pb.jpg')} alt=""className="firstpic" />
       <div className="contents media">
       <h1>Media</h1>
       <div className="mediaPlayer">
       <ReactPlayer className="videoPreview" url={'https://vimeo.com/333159305'} controls/>
-
+      <ReactPlayer className="videoPreview" url={'https://vimeo.com/384878504'} controls/>
       </div>
       
       
